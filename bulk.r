6ba4516a37840eaf2c96ec16ac9e4ed5c1e1ca66
simonmc <simonmc@coginov.com>
2018-11-16 14:32:42 -0500

    [maint] Added csv-to-bulk and bulk-to-csv functions that enables the creation of a bulk from csv data and to write a bulk in csv format.

--- bulk.r
+++ bulk.r
@@ -103,7 +103,7 @@ REBOL [
 		
 			any WORD! or LIT-WORD! value in the header is expected to be used litteraly, not as a bound,
 			evaluatable word.  Because of this, PRINTing the header might provoke errors, you should 
-			always PROBE it.  In R3,  the header will be explicitely unbound.
+			always PROBE it.  In Red,  the header will be explicitely unbound.
 		
 			You can obviously have BULK items in a BULK, hence its nested aspect.
 			
@@ -121,7 +121,7 @@ REBOL [
 		 because bulk is a simple format to exchange data between arbitrary tools, some include databases
 		 and huge but simple datasets.
 		
-		-Generic tools for convertion, disk & networking can safely convert any bulk with minimal fuss,
+		-Generic tools for conversion, disk & networking can safely convert any bulk with minimal fuss,
 		 including the header.
 		
 		-Programmatically, the header is easily avoided by simply skipping it, and can then be used as a
@@ -190,7 +190,7 @@ REBOL [
 		NOTES:
 			-the header IS NOT an object on purpose.
 		
-			-in R3 it will become a map!
+			-in Red it will become a map!
 		
 	}
 	;-  \ documentation
@@ -298,9 +298,412 @@ REBOL [
 ;--------------------------------------
 
 slim/register [
+	;-                                                                                                       .
+	;-----------------------------------------------------------------------------------------------------------
+	;
+	;- PARSING RULES
+	;
+	;-----------------------------------------------------------------------------------------------------------
+	
+	;--------------------------
+	;-     	whitespaces:
+	;
+	;--------------------------
+	=space=: charset [ #" " #"^(A0)" #"^(8D)"   #"^(8F)"   #"^(90)" ]
+	=tab=: charset "^-"
+	=spacer=: union =space= =tab=
+	=spacers=: [some =spacer=]
+	=nl=: [ opt =cr= =lf= (++ .line-count)]
+	=cr=: #"^M"
+	=lf=:  #"^/" 
+	
+	;--------------------------
+	;-     	collectors:
+	;
+	;--------------------------
+	!collect!: [(append .value .txt)]
+	!collect-value!: [(append .row copy .value clear .value)]
+
+	;--------------------------
+	;-     	=separator=:
+	;
+	;--------------------------
+	=separator=: charset ","
+
+
+	;--------------------------
+	;-     	=quoted-chars=:
+	;
+	;--------------------------
+	=quoted-chars=: complement charset {"^/^M} ; note we consider the ^/ directly in the rule, to increment line number
+	;--------------------------
+	;-     	=unquoted-chars=:
+	;
+	;--------------------------
+	=unquoted-chars=: complement union =separator= charset {^/^M}
+
+	;--------------------------
+	;-     	=qvalue=:
+	;
+	;--------------------------
+	=qvalue=: [
+		{"} 
+		;(print {"})
+		some [ 
+			  [{""} (append .value {"})]
+			| [
+				copy .txt 
+				some [
+					=quoted-chars=  
+					| =lf= (++ .line-count)
+				]
+				!collect! 
+			]
+		]
+		{"}
+	]
+	;--------------------------
+	;-     	=uqvalue=:
+	;
+	;--------------------------
+	=uqvalue=: [
+		copy .txt some =unquoted-chars= !collect!
+	]
+
+	;--------------------------
+	;-     	=value=:
+	;
+	;--------------------------
+	=value=: [
+		[
+		 	 =qvalue=  ;(prin "Q  - ")
+			| =uqvalue= ;(prin "UQ - ")
+		]
+		;(print .value)
+	]
+
+	;--------------------------
+	;-     	=row=:
+	;
+	;--------------------------
+	=row=:  [
+		=value=  !collect-value! 
+		any [
+			=separator= ;(print "separator")
+			=value= !collect-value!  
+		]
+		
+	]
+
+
+	
+	
+	;-                                                                                                       .
+	;-----------------------------------------------------------------------------------------------------------
+	;
+	;- BINDINGS
+	;
+	;-----------------------------------------------------------------------------------------------------------
+	; declare words to bind locally to context
+	;--------------------------
+	;-     	.table:
+	;
+	;--------------------------
+	.table: []
+	;--------------------------
+	;-     	.row:
+	;
+	;--------------------------
+	.row: []
+	;--------------------------
+	;-     	.value:
+	;
+	;--------------------------
+	.value: ""
+	;--------------------------
+	;-     	.here:
+	;
+	;--------------------------
+	.here: .txt: none
+	;--------------------------
+	;-     	.column-count:
+	;
+	;--------------------------
+	.column-count: .old-column-count: none
+	;--------------------------
+	;-     	.line-count:
+	;
+	;--------------------------
+	.line-count: 0
+	;--------------------------
+	;-     	parsed-all?:
+	;
+	;--------------------------
+	parsed-all?: false	
+	
+	;-                                                                                                       .
+	;-----------------------------------------------------------------------------------------------------------
+	;
 	;- FUNCTIONS
+	;
+	;-----------------------------------------------------------------------------------------------------------
 	
 	all*: :all
+	
+	;--------------------------
+	;-     parse-csv()
+	;--------------------------
+	; purpose:  Parses a csv string and returns its data in a block
+	;
+	; inputs:   
+	;
+	; returns:  
+	;
+	; notes:    
+	;
+	; to do:    
+	;
+	; tests:    
+	;--------------------------
+	parse-csv: funcl [
+		data	[string!]	"The data to parse"
+		
+		/tag-row-end	"Will add ___ROW-END___ at the end of each rows in the table result"
+		/extern .table .row .value .here .txt .column-count .old-column-count .line-count parsed-all?
+	][
+		; We want to reinitialize context between each call
+		.table: copy []
+		.row: copy []
+		.value: copy ""
+		.here: .txt: none
+		.column-count: .old-column-count: none
+		.line-count: 0
+		parsed-all?: false
+		
+		; Parsing
+		parsed-all?: parse/all data [
+			some[
+				.here:
+				;(print .here)
+				;(print first .here)
+				[
+					[
+						=row= ;(print "END ROW")
+						[ =nl= | end ]
+						(
+							
+							if tag-row-end [
+								append .row '___ROW-END___
+							]
+							;++ .line-count
+							.old-column-count: .column-count
+							.column-count: length? .row
+							;(?? .column-count)
+							
+							all [
+								.old-column-count
+								.column-count <> .old-column-count
+								to-error rejoin ["column count mismatch! (line ".line-count ") was : " .old-column-count  " found : " .column-count ]
+							]
+							;?? .column-count
+							;.column-count: 
+							append .table .row 
+							clear .row  
+							;probe "____ROW____"
+						)
+					]
+					;---
+					; note that we do not accumulate blank lines in the result dataset.
+					| opt =spacers= =nl= (
+						;++ .line-count
+						;probe "____BLANK LINE____" 
+					)
+				]
+				
+			]
+		]
+
+		;new-line/skip .table true .column-count ; <SMC> Crashes with empty string (I think...)
+		
+		copy .table
+	]
+	
+	;--------------------------
+	;-     csv-to-bulk()
+	;--------------------------
+	; purpose:  Converts a csv string or file to a bulk
+	;
+	; inputs:   
+	;
+	; returns:  
+	;
+	; notes:    
+	;
+	; to do:    
+	;
+	; tests:    
+	;--------------------------
+	csv-to-bulk: funcl [
+		csv-data	[string! file! binary!]	"Path of the csv file, or its binary|string content"
+		/header								"Will store the first row as bulk labels"
+	][
+		csv-data: switch type?/word csv-data [
+			file! [
+				; If given a file, read the file to get its content to parse
+				to-string read/binary csv-data
+			]
+			binary! [
+				; If given a binary, convert it to a string for parsing
+				to-string csv-data
+			]
+			string! [
+				; If given a string, take it as is
+				csv-data
+			]
+		]
+		
+		; - Data integrity verification
+		; If the data has more than one row, it should contain at least one crlf
+		; The absence of clrf might indicate a wrong file reading => WARNING
+		unless find csv-data crlf [
+			print "==============================================================="
+			print "WARNING! (csv-to-bulk): No crlf found in data"
+			print {Please use [to-string read/binary <file-path>] to read the file}
+			print "If the data contains only one row, you can ignore this warning"
+			print "==============================================================="
+		]
+		
+		parsed-result: parse-csv csv-data
+		
+		?? parsed-result
+		
+		either header [
+			; Extract header row from the data
+			head-row: copy/part parsed-result .column-count
+			remove/part parsed-result .column-count
+			
+			; Generate labels
+			lbl-lit-words: copy []
+			foreach lbl head-row [repend lbl-lit-words to-lit-word lbl]
+			labels: compose/only [labels: (lbl-lit-words)]
+						
+			make-bulk/records/properties .column-count parsed-result labels
+		][
+			make-bulk/records .column-count parsed-result
+		]
+	]
+	
+	;--------------------------
+	;-     bulk-to-csv()
+	;--------------------------
+	; purpose:  Generate a csv writable string from a bulk
+	;
+	; inputs:   
+	;
+	; returns:  
+	;
+	; notes:
+	;	- If the given bulk has a labels property, it is added as the first row
+	;
+	;	=CONCERNING THE CSV OUTPUT=
+	;	(In following, by 'wrapped, we mean that the content is surrounded by double-quotes ("))
+	;	- Content containing at least one of
+	;		1. newline (lf)
+	;		2. double-quote (")
+	;		3. comma (,)
+	;	  is wrapped
+	;	- All double-quotes (") in a wrapped content are escaped with a double-quote
+	;	- The newlines in wrapped content must be a lf and not a crlf
+	;		e.g. text^M^/text -> "text^/text"
+	;			where (^M^/ = clrf) and (^/ = lf)
+	; 	  The rows must end by a crlf (except the last one)
+	;	- In all other cases, content appears as is without wrapping
+	;	- Examples
+	;		{text text}		-> {text text}
+	;		{text,text}		-> {"text,text"}
+	;		{text^/text}	-> {"text^/text"}
+	;		{text^M^/text}	-> {"text^/text"}
+	;		{text"text}		-> {"text""text"}
+	;		{"text"}		-> {"""text"""}
+	; to do:    
+	;
+	; tests:    
+	;--------------------------
+	bulk-to-csv: funcl [
+		blk						[block!]
+		/write-to	output-file [file!]
+	][
+		result: ""
+		
+		; Manage header
+		if labels: get-bulk-property bulk-test 'labels [
+			foreach lbl labels [
+				repend result [to-csv-content to-string lbl ","]
+			]
+			remove back tail result ; Remove trailing comma
+			append result crlf
+		]
+		
+		; Manage content rows
+		; <smc> Might not be efficient
+		col-nbr: bulk-columns blk
+		blk-data: next blk ;skip metadata
+		forskip blk-data col-nbr [
+			c-row: copy/part blk-data col-nbr
+			foreach cell c-row [
+				repend result [to-csv-content cell ","]
+			]
+			remove back tail result ; Remove trailing comma
+			append result crlf
+		]
+		
+		remove back tail result
+		remove back tail result ; Remove trailing crlf
+		
+		if write-to [
+			write/binary output-file to-binary result
+		]
+		
+		result
+	]
+	
+	;--------------------------
+	;-     to-csv-content()
+	;--------------------------
+	; purpose:  Takes a string and convert it to a csv valid form
+	;
+	; inputs:   
+	;
+	; returns:  
+	;
+	; notes:   - See documentation of bulk-to-csv() 
+	;			- All crlf are converted to lf
+	;
+	; to do:    
+	;
+	; tests:    
+	;--------------------------
+	to-csv-content: funcl [
+		content	[string!]
+	][
+		wrap: either any [
+			find content #"^""
+			find content #","
+			find content lf
+		][true][false]
+		
+		; Replace all crlf by lf
+		replace/all content crlf lf
+		
+		; Escape double-quotes
+		replace/all content {"} {""}
+		
+		if wrap [
+			content: rejoin [{"} content {"}]
+		]
+		
+		content
+	]
 	
 	;-----------------
 	;-     is-bulk?
@@ -947,6 +1350,8 @@ slim/register [
 		]
 		;vout
 	]
+	
+	
 	
 ]
 
